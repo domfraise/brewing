a259ac4c0f57fad3fd14cbf02a5d168ad2c5f7e8
domfraise <domfraise@gmail.com>
2020-11-11 23:27:12 +0000

    add max restr

--- salts-to-ppm-v2.py
+++ salts-to-ppm-v2.py
@@ -24,11 +24,31 @@ class Ion:
     def get_ppm_for_weight(self, weight):
         return self.ppmPerGramme * weight
 
+class MaxRestriction:
+    def __init__(self, ion_name, max_ppm):
+        self.ion_name = ion_name
+        self.max_ppm = max_ppm
+
+    def violates_restriction(self, salt_solution):
+        ppm_for_ion = salt_solution.get_current_ppms()[self.ion_name]
+        if ppm_for_ion is not None and ppm_for_ion > self.max_ppm:
+            return True
+        else:
+            return False
+
+
 class SaltSolution:
-    def __init__(self, salt_defs):
+    def __init__(self, salt_defs, max_restrictions):
         self.salt_concentrations = []
         self.salt_defs = salt_defs
+        self.max_restrictions = max_restrictions
         self.ion_sources = self.calculateIonSources()
+
+    def solution_is_valid(self):
+        for restriction in max_restrictions:
+            if restriction.violates_restriction(self):
+                return False
+        return True
 
     def get_current_ppms(self):
         ionPpms = {}
@@ -48,14 +68,33 @@ class SaltSolution:
                 return
         self.salt_concentrations.append(SaltConcentration(salt_def, weight))
 
+    def remove_salt(self, salt_def, weight):
+        for concentration in self.salt_concentrations:
+            if concentration.salt_def.name is salt_def.name:
+                concentration.weight -= weight
+                if concentration.weight <= 0:
+                    self.salt_concentrations.remove(concentration)
+
     def add_salt_for_ppm(self, ion_name, ppm_to_add):
         salt_def = salt_defs[self.ion_sources[ion_name][0]]
+        for salt in self.ion_sources[ion_name]:
+            weight_aded = self.add_ppm_from_specific_source(ion_name, ppm_to_add, salt_def)
+            if weight_aded > 0:
+                break
+
+            #ion.ppm * x = ppm_add
+        #attempt partial add
+    def add_ppm_from_specific_source(self, ion_name, ppm_to_add, salt_def):
+        weight_to_add = 0
         for ion in salt_def.ions:
             if ion.name == ion_name:
-                weight_to_add = ppm_to_add/ion.ppmPerGramme
-                self.addSalt(salt_def, weight_to_add) #?
-                #ion.ppm * x = ppm_add
+                weight_to_add = ppm_to_add / ion.ppmPerGramme
+                self.addSalt(salt_def, weight_to_add)  # ?
+                if not self.solution_is_valid():
+                    self.remove_salt(salt_def, weight_to_add)
+                weight_to_add = 0
 
+        return weight_to_add
 
     def calculateIonSources(self):
         ionSources = {}
@@ -75,7 +114,7 @@ salt_defs = {"CaSO4": SaltDef("CaSO4", [Ion("ca", 23), Ion("s04", 56)]),
          "NaHCO3": SaltDef("NaHCO3", [Ion("na", 27), Ion("hc03", 73)])}
 
 
-
+max_restrictions = [MaxRestriction("ca", 100), MaxRestriction("mg", 50), MaxRestriction("na", 150), MaxRestriction("s04", 400), MaxRestriction("cl", 400), MaxRestriction("hc03", 20)]
 
 soln = SaltSolution(salt_defs)
 
